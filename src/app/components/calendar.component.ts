faf3e41948f30410e2c7a2aef24fa9ba13f2c324
tenglongroy <379600075@qq.com>
2021-06-03 01:27:39 +1000

    calculate days in month

--- src/app/components/calendar.component.ts
+++ src/app/components/calendar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { AppState } from 'src/app/models';
+import { StaticMethodService } from '../shared/common-utilities';
 
 @Component({
     selector: 'app-calendar',
@@ -9,7 +10,8 @@ import { AppState } from 'src/app/models';
 })
 export class CalendarComponent implements OnInit, OnDestroy {
 
-    constructor(private store: Store<AppState>) { }
+    constructor(private store: Store<AppState>,
+                private staticMethod: StaticMethodService) { }
 
     ngOnInit(): void {
     }
--- src/app/shared/common-utilities.ts
+++ src/app/shared/common-utilities.ts
@@ -1,9 +1,62 @@
+import { Injectable } from "@angular/core";
 import { RippleGlobalOptions } from "@angular/material/core";
 
 export const globalRippleConfig: RippleGlobalOptions = {
     animation: {
-      enterDuration: 300,
-      exitDuration: 500
+        enterDuration: 300,
+        exitDuration: 500
     },
     terminateOnPointerUp: false
 };
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StaticMethodService {
+    constructor() {
+
+    }
+
+    generateMonthDayArray(): any[] {
+        const now = new Date();
+        const day = now.getDate();
+        const month = now.getMonth();
+        const year = now.getFullYear();
+        const days = this.getMonthDays(month + 1, year);
+        const firstDay = new Date(year, month, 1);
+        console.log(firstDay, days)
+        const startDay = firstDay.getDay();
+        console.log(startDay, 'the starting day')
+        return this.setThePrintLoop(startDay, days)
+    }
+    getMonthDays(month, year): number {
+        let days;
+        if ([1, 3, 5, 7, 8, 10, 12].includes(month)){
+            days = 31;
+        }
+        else if ([4, 6, 9, 11].includes(month)){
+            days = 30;
+        }
+        else if (month === 2) {
+            if (year % 4 === 0) {
+                days = 29;
+            }
+            else {
+                days = 28;
+            }
+        }
+        return days;
+    }
+    setThePrintLoop(s, d): any[] {
+        const DayArray = []
+        for (let i = 0; i < 35; i++) {
+            if (i >= (s) && (i - s) < 31) {
+                DayArray.push(i - s + 1);
+            }
+            else {
+                DayArray.push('');
+            }
+        }
+        return DayArray;
+    }
+}
